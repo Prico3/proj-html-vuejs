5e64db416772e76f6185b9c5850c9bb7384a5076
Prico3 <pricoco.andrea@gmail.com>
2022-11-17 15:19:12 +0100

    added last img

---
+++ src/components/TopInfo.vue
@@ -0,0 +1,44 @@
+<script>
+export default {}
+</script>
+
+<template>
+
+    <div class="info">
+
+        <div class="container d-flex justify-content-between align-items-center">
+
+            <div class="left_info">
+                ciao
+            </div>
+
+            <div class="right_info">
+                ciao
+            </div>
+
+        </div>
+
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables";
+
+.info {
+    //debug
+    height: 50px;
+    color: black;
+}
+
+.left_info {
+    //debug
+    border: 1px solid black;
+
+}
+
+.right_info {
+    //debug
+    border: 1px solid green;
+}
+</style>

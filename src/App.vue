12506ffa53031815c6e1137812c7873734d55cc6
Prico3 <pricoco.andrea@gmail.com>
2022-11-17 17:58:49 +0100

    services app layout

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,37 @@
 import TopInfoVue from './components/TopInfo.vue';
 import HeaderAppVue from './components/HeaderApp.vue';
 import JumboAppVue from './components/JumboApp.vue';
+import ServicesAppVue from './components/ServicesApp.vue';
 
 export default {
   components: {
     TopInfoVue,
     HeaderAppVue,
     JumboAppVue,
+    ServicesAppVue,
 
   }
 }
 </script>
 
 <template>
+
   <TopInfoVue />
+
   <div class="ciao">
     <HeaderAppVue />
     <JumboAppVue />
-
   </div>
+
+  <ServicesAppVue />
+
+
 </template>
 
 <style lang="scss">
 @use "./styles/general.scss" as *;
 
 .ciao {
-  background-color: aquamarine;
+  background-image: url("./assets/img/bg-jumbo.webp");
 }
 </style>
---
+++ src/components/ServicesApp.vue
@@ -0,0 +1,76 @@
+<script>
+export default {}
+</script>
+
+<template>
+    <div class="ms_container">
+        <p>OUR BUSINESS AREAS</p>
+        <h2>Excellence in <a href="">Services</a></h2>
+
+        <div class="text d-flex justify-content-between">
+            <p>We are leaders in providing consultancy servivces with a set of cutting-edge technologies and a team of a
+                experienced and renowned professionals. These are some options that you can hire.</p>
+            <button>SEE ALL</button>
+        </div>
+
+        <div class="wrapper">
+
+            <div class="row row-cols-3">
+
+                <div class="col">
+                    <div class="ms_card">
+                        <i class="fa-solid fa-briefcase"></i>
+                        <h4>Audit & Assurance</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="ms_card">
+                        <i class="fa-solid fa-briefcase"></i>
+                        <h4>Audit & Assurance</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="ms_card">
+                        <i class="fa-solid fa-briefcase"></i>
+                        <h4>Audit & Assurance</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="ms_card">
+                        <i class="fa-solid fa-briefcase"></i>
+                        <h4>Audit & Assurance</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="ms_card">
+                        <i class="fa-solid fa-briefcase"></i>
+                        <h4>Audit & Assurance</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="ms_card">
+                        <i class="fa-solid fa-briefcase"></i>
+                        <h4>Audit & Assurance</h4>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+</style>

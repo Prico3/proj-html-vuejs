99a4c690d0ee58cbfed2aacbeb809981f35f12be
Prico3 <pricoco.andrea@gmail.com>
2022-11-21 16:12:37 +0100

    results completed

--- src/components/ProjectsApp.vue
+++ src/components/ProjectsApp.vue
@@ -1,12 +1,43 @@
 <script>
-export default {}
+export default {
+    data() {
+        return {
+            info: [
+                {
+                    text: "Academic professional program in social media",
+                    bg: 'project-1.jpg',
+                },
+                {
+                    text: "President's speech at the annual meeting"
+                },
+                {
+                    text: "International business trip in Shanghai"
+                },
+                {
+                    text: "Technology workshop with education theme"
+                },
+                {
+                    text: "Donation og clothes and food to the partner NGO"
+                },
+                {
+                    text: "Confraternization of the procurment team"
+                },
+            ]
+        }
+    },
+    computed: {
+        bgImg() {
+            return `background-image: url("../assets/img/project-1.jpg");`;
+        }
+    }
+}
 </script>
 
 <template>
     <section>
         <div class="ms_container text-center">
-            <p>WE DO MORE FOR EVERYONE</p>
-            <h2>Actions & <a href="">Projects</a></h2>
+            <p class="smaller_title">WE DO MORE FOR EVERYONE</p>
+            <h2 class="section_title">Actions & <a class="primary_section_title" href="">Projects</a></h2>
             <ul class="d-flex justify-content-center">
 
                 <li><a href="">ALL</a></li>
@@ -17,41 +48,19 @@ export default {}
                 <li><a href="">ENVIROMENT</a></li>
                 <li><a href="">TECHNOLOGY</a></li>
 
+
             </ul>
 
             <div class="row row-cols-3">
 
 
-                <div class="col">
+                <div class="col" v-for="item in info" :style="bgImg">
                     <div class="ms_card">
-                        Academic professional program in social media
+                        <p>{{ item.text }}</p>
+
                     </div>
                 </div>
-                <div class="col">
-                    <div class="ms_card">
-                        Academic professional program in social media
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="ms_card">
-                        Academic professional program in social media
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="ms_card">
-                        Academic professional program in social media
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="ms_card">
-                        Academic professional program in social media
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="ms_card">
-                        Academic professional program in social media
-                    </div>
-                </div>
+
 
             </div>
         </div>
@@ -67,5 +76,8 @@ export default {}
 
 ul li {
     margin: 3rem 0.3rem;
+    font-size: smaller;
 }
+
+.ms_card {}
 </style>
